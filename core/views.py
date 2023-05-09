2aee71ccb4d73dfa81292bcdf9b52b0524cc7581
Tinatin <omurovatinatin@gmail.com>
2023-05-09 18:18:19 +0600

    add produts

--- core/views.py
+++ core/views.py
@@ -3,15 +3,30 @@ from .models.auth import *
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.views import View
-
+from .models.product import Product
+from .models.category import Category
 
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'base.html')
 
+        products = Product.get_all_products()
+        categories = Category.get_all_categories()
+        categoryID = 'category'
+        if 'category' in request.GET:
+            categoryID = request.GET['category']
+        else:
+            categoryID = False
+        if categoryID:
+            products = Product.get_all_products_by_categoryId(categoryID)
+        else:
+            products = Product.get_all_products()
+        data = {}
+        data['products'] = products
+        data['categories'] = categories
+        return render(request, 'core/products.html', data)
+    
 
 @login_required(login_url='signin/')
-
 
 
 def login_view(request):
