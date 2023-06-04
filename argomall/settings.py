f860b09e0f1c77492a6687aa5d6b0e43ec5e1148
Tinatin <omurovatinatin@gmail.com>
2023-06-04 20:58:00 +0600

    fixed bugs

--- argomall/settings.py
+++ argomall/settings.py
@@ -140,8 +140,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'loveyourselfbegin@gmail.com'
-EMAIL_HOST_PASSWORD = 'vqohweshxmoruobh'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
